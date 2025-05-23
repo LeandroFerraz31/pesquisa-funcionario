bb57e9519fa62539d338edd5400aedaa805a85b7
LeandroFerraz31 <leandro.santos@universo.univates.br>
2025-05-23 12:45:54 -0300

    ajuste feitos no server.js para rodar no square cloud

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 // Middleware
 app.use(bodyParser.json());
